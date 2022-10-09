38c5a8504ec39ef6265c345a9c801867d5b36b18
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-09 08:41:22 -0500

    Error comun con Cookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,39 @@
 import '../styles/globals.css';
 import type { AppContext, AppProps } from 'next/app';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, Theme, ThemeProvider } from '@mui/material';
 import { darkTheme, customTheme, lightTheme } from '../themes';
+import Cookies from 'js-cookie';
 
-function MyApp({ Component, pageProps, ...rest }: AppProps) {
-  console.log(`🚀 rest:`, rest);
+interface Props extends AppProps {
+  theme: string;
+}
+
+function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
+  const cookieTheme = Cookies.get('theme') || 'light'; // Error: Warning: Prop `className` did not match. Server: ...
+
+  console.log({cookieTheme});
+
+  const currentTheme: Theme = cookieTheme === 'light'
+    ? lightTheme
+    : cookieTheme === 'dark'
+      ? darkTheme
+      : customTheme;
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
 
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  const {theme} = appContext.ctx.req ? (appContext.ctx.req as any).cookies : { theme: 'light' };
-  const validThemes = ['light', 'dark', 'custom'];
-  // console.log('getInitialProps:', cookies);
-  return {
-    theme: validThemes.includes(theme) ? theme : 'dark',
-  }
-};
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   const { theme } = appContext.ctx.req ? (appContext.ctx.req as any).cookies : { theme: 'light' };
+//   const validThemes = ['light', 'dark', 'custom'];
+//   // console.log('getInitialProps:', cookies);
+//   return {
+//     theme: validThemes.includes(theme) ? theme : 'dark',
+//   };
+// };
 
 export default MyApp;
